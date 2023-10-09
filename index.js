3a7a7689cc1fa7d20a98e31f15c48a95df4a4977
Ahmed Salem <as2720094@gmail.com>
2023-10-10 02:03:19 +0300

    Create the upload route

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ httpServer.on("request", (req, res) => {
     fs.readFileSync("index.html");
     return;
   }
+
+  if (req.url === "/upload") {
+    const fileName = req.headers["file-name"];
+
+    req.on("data", (data) => {
+      fs.appendFileSync(fileName, data);
+      console.log("received data: " + data.length);
+    });
+    res.end("Uploaded");
+  }
 });
 
 httpServer.listen(3300);
